5b34db017dd52671abc54a0dc9aa960a7cc625ac
BabaDyaga <yagatech@outlook.com>
2023-04-16 13:45:07 +0100

    sh file deletion

--- debug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-gcc  -g -o Dynammic_Array.o main.c Dynammic_Array.c
-
-gdb ./Dynammic_Array.o
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-gcc -o Dynammic_Array.o Dynammic_array_Example.c Dynammic_Array.c
-
-./Dynammic_Array.o
